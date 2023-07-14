66e24c0d18d19c2f4259fa239806a2593c0f3c0d
Yashniboriya <niboriyadeepali109@gmail.com>
2023-07-14 14:34:01 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Footer = () => {
+  return <footer>
+    <div>
+        <h1>TechyStar</h1>
+        <p>@All rights reserved</p>
+    </div>
+
+    <div>
+        <h5>Follow us</h5>
+        <div>
+            <a href="http://https//github.com/Yashniboriya" target={"blank"}>Github</a>
+            <a href="niboriyayash7@gmail.com" target={"blank"}>Gmail</a>
+            <a href="http://https//www.linkedin.com/in/yash-niboriya-613918213/" target={"blank"}>LinkedIn</a>
+        </div>
+    </div>
+  </footer>
+}
+
+export default Footer
